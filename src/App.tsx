fd62cebe6d1ff453b1dba88bbf2ef5ca3dfc37b8
genics85 <eugeneamo85@gmail.com>
2024-04-29 20:40:20 +0000

    select and select all included

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+
+type Token = {
+  id: number;
+  name: string;
+  amount: string;
+};
+
+const tokens: Token[] = [
+  { id: 1, name: "Dogwhif", amount: "1.4" },
+  { id: 2, name: "Dogwhif", amount: "1.4" },
+];
 
 function App() {
   const [swap, setSwap] = useState<boolean>(false);
   const [wallet, setWallet] = useState<boolean>(false);
+  const [selected, setSelected] = useState<number[]>([]);
+  const [selectAll, setSelectAll] = useState<boolean>();
 
   const handleOnClick = () => {
     if (wallet) {
@@ -12,6 +25,30 @@ function App() {
     setWallet(true);
   };
 
+  const handleSelect = (id: number) => {
+    setSelectAll(false);
+    if (selected.includes(id)) {
+      let newSelected = selected.filter((item) => item != id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([...selected, id]);
+  };
+
+  const handleSelectAll = (e: ChangeEvent<HTMLInputElement>) => {
+    let isChecked = e.target.checked;
+    console.log(isChecked);
+
+    if (isChecked) {
+      let ids = tokens.map((item, i) => item.id);
+      setSelectAll(true);
+      setSelected(ids);
+      return;
+    }
+    setSelectAll(false);
+    setSelected([]);
+  };
+
   return (
     <div className=" text-gray-600 px-4 font-semibold w-screen h-screen gap-10  flex flex-col md:flex-row items-center justify-center">
       <div className=" p-10 w-full  md:w-[500px] h-[400px] border-[3px] rounded-md border-gray-500">
@@ -19,15 +56,36 @@ function App() {
           <div className="flex flex-col gap-5 items-center">
             <p>Token Name</p>
             <div className="items flex flex-col gap-4">
-              <p>Dogwhif</p>
-              <p>Dogwhif</p>
+              <div className="flex gap-3">
+                <input
+                  onChange={(e) => handleSelectAll(e)}
+                  checked={selectAll}
+                  className="acc accent-slate-500"
+                  type="checkbox"
+                />
+                <p>All</p>
+              </div>
+              {tokens.map((token, i) => {
+                return (
+                  <div key={i} className="flex gap-2">
+                    <input
+                      onChange={() => handleSelect(token.id)}
+                      checked={selected.includes(token.id)}
+                      className="acc accent-slate-500"
+                      type="checkbox"
+                    />
+                    <p>{token.name}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div className="flex flex-col gap-5 items-center">
             <p>Amount</p>
             <div className="items flex flex-col gap-4">
-              <p>1.4M</p>
-              <p>1.4M</p>
+              {tokens.map((token, i) => {
+                return <p key={i}>{token.amount}</p>;
+              })}
             </div>
           </div>
         </div>
